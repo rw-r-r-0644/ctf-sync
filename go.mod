64e37c619d33055a95e46d53da6ecf8a54cc7ce4
rw-r-r-0644 <rw-r-r-0644@protonmail.com>
2026-01-20 18:57:40 +0100

    add automatic response decompression for gzip, deflate, br, and zstd

--- go.mod
+++ go.mod
@@ -5,8 +5,8 @@ go 1.24.0
 require github.com/refraction-networking/utls v1.8.2
 
 require (
-	github.com/andybalholm/brotli v1.0.6 // indirect
-	github.com/klauspost/compress v1.17.4 // indirect
+	github.com/andybalholm/brotli v1.2.0 // indirect
+	github.com/klauspost/compress v1.18.3 // indirect
 	golang.org/x/crypto v0.47.0 // indirect
 	golang.org/x/net v0.49.0 // indirect
 	golang.org/x/sys v0.40.0 // indirect
--- internal/tls/client.go
+++ internal/tls/client.go
@@ -1,44 +1,111 @@
 package tls
 
 import (
+	"compress/flate"
+	"compress/gzip"
 	"context"
 	"crypto/tls"
+	"io"
 	"net"
 	"net/http"
 	"time"
 
+	"github.com/andybalholm/brotli"
+	"github.com/klauspost/compress/zstd"
 	"golang.org/x/net/http2"
 
 	utls "github.com/refraction-networking/utls"
 )
 
-// match firefox fingerprint for now
+// decompressingTransport wraps an http.RoundTripper and automatically decompresses responses
+type decompressingTransport struct {
+	rt http.RoundTripper
+}
+
+func (t *decompressingTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	resp, err := t.rt.RoundTrip(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// Decompress based on Content-Encoding
+	switch resp.Header.Get("Content-Encoding") {
+	case "gzip":
+		gr, err := gzip.NewReader(resp.Body)
+		if err != nil {
+			resp.Body.Close()
+			return nil, err
+		}
+		resp.Body = &readCloser{Reader: gr, Closer: resp.Body}
+		resp.Header.Del("Content-Encoding")
+		resp.ContentLength = -1
+		resp.Uncompressed = true
+	case "deflate":
+		resp.Body = &readCloser{Reader: flate.NewReader(resp.Body), Closer: resp.Body}
+		resp.Header.Del("Content-Encoding")
+		resp.ContentLength = -1
+		resp.Uncompressed = true
+	case "br":
+		resp.Body = &readCloser{Reader: brotli.NewReader(resp.Body), Closer: resp.Body}
+		resp.Header.Del("Content-Encoding")
+		resp.ContentLength = -1
+		resp.Uncompressed = true
+	case "zstd":
+		zr, err := zstd.NewReader(resp.Body)
+		if err != nil {
+			resp.Body.Close()
+			return nil, err
+		}
+		resp.Body = &readCloser{Reader: zr, Closer: resp.Body}
+		resp.Header.Del("Content-Encoding")
+		resp.ContentLength = -1
+		resp.Uncompressed = true
+	}
+
+	return resp, nil
+}
+
+// readCloser combines an io.Reader and io.Closer
+type readCloser struct {
+	io.Reader
+	Closer io.Closer
+}
+
+func (rc *readCloser) Close() error {
+	return rc.Closer.Close()
+}
+
+// NewClient returns a new http.Client that uses utls to mimic a real browser (Firefox).
+// This helps bypass WAFs that fingerprint the standard Go TLS client.
+// The client automatically handles response decompression for gzip, deflate, br, and zstd.
 func NewClient() *http.Client {
 	return &http.Client{
-		Transport: &http2.Transport{
-			DialTLS: func(network, addr string, cfg *tls.Config) (net.Conn, error) {
-				dialer := net.Dialer{Timeout: 30 * time.Second}
-				conn, err := dialer.DialContext(context.Background(), network, addr)
-				if err != nil {
-					return nil, err
-				}
+		Transport: &decompressingTransport{
+			rt: &http2.Transport{
+				DialTLS: func(network, addr string, cfg *tls.Config) (net.Conn, error) {
+					dialer := net.Dialer{Timeout: 30 * time.Second}
+					conn, err := dialer.DialContext(context.Background(), network, addr)
+					if err != nil {
+						return nil, err
+					}
 
-				host, _, err := net.SplitHostPort(addr)
-				if err != nil {
-					host = addr
-				}
+					host, _, err := net.SplitHostPort(addr)
+					if err != nil {
+						host = addr
+					}
 
-				uConn := utls.UClient(conn, &utls.Config{
-					ServerName: host,
-					NextProtos: []string{"h2", "http/1.1"},
-				}, utls.HelloFirefox_Auto)
+					uConn := utls.UClient(conn, &utls.Config{
+						ServerName: host,
+						NextProtos: []string{"h2", "http/1.1"},
+					}, utls.HelloFirefox_Auto)
 
-				if err := uConn.Handshake(); err != nil {
-					_ = conn.Close()
-					return nil, err
-				}
+					if err := uConn.Handshake(); err != nil {
+						_ = conn.Close()
+						return nil, err
+					}
 
-				return uConn, nil
+					return uConn, nil
+				},
 			},
 		},
 		Timeout: 30 * time.Second,
